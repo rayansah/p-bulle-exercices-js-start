b5a24f13c31985516d41edb519e3c3708ed6c73e
rayansah <pa62yjh@gmail.com>
2026-01-22 15:41:32 +0100

    chore(annalyns-infiltration): [45] [DONE]

--- exercices/annalyns-infiltration/annalyns-infiltration.js
+++ exercices/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,8 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  if (knightIsAwake == true) return false
+  else return true
 }
 
 /**
@@ -40,7 +41,7 @@ export function canExecuteFastAttack(knightIsAwake) {
  * @returns {boolean} Whether or not you can spy on someone.
  */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  return knightIsAwake || archerIsAwake || prisonerIsAwake
 }
 
 /**
@@ -52,7 +53,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  return !archerIsAwake && prisonerIsAwake
 }
 
 /**
@@ -71,5 +72,6 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent,
 ) {
-  throw new Error('Remove this line and implement the function');
+  if (petDogIsPresent) return !archerIsAwake
+  return !knightIsAwake && !archerIsAwake && prisonerIsAwake 
 }
